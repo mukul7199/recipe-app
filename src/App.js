9ebb60955f46dace32f12e3ee6a2e8761496c725
mukul7199 <mukul7199@gmail.com>
2019-03-13 16:58:28 +0530

    Added login

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Recipes from "./Recipes";
 import Home from "./Home";
 import AddRecipe from "./AddRecipe";
 import Signup from "./Signup";
-//import Login from "./Login";
+import AuthenticatedComponent from "./AuthenticatedComponent";
+import Login from "./Login";
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
           <Navbar />
-
           <Switch>
             <Route exact path="/" component={Home} />
-
             <Route exact path="/recipes" component={Recipes} />
-            <Route path="/recipes/add" component={AddRecipe} />
             <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <AuthenticatedComponent>
+              <Route path="/recipes/add" component={AddRecipe} />
+            </AuthenticatedComponent>
           </Switch>
         </div>
       </BrowserRouter>
